26804c49e66d172a34cc8087745129da8889f413
SnO2WMaN <me@sno2wman.net>
2021-06-29 22:54:27 +0900

    feat: 日本語対応?

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -2,20 +2,25 @@ import chrome from 'chrome-aws-lambda';
 import core from 'puppeteer-core';
 import {ParsedOptions} from './parser';
 
-export const getBrowserOption = async () =>
+export const getBrowserOption = async () => {
   // eslint-disable-next-line no-process-env
-  process.env.AWS_LAMBDA_FUNCTION_VERSION
-    ? {
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless,
-      }
-    : {
-        args: [],
-        // eslint-disable-next-line no-process-env
-        executablePath: process.env.CHROME_EXECUTABLE_PATH,
-        headless: true,
-      };
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    await chrome.font(
+      'https://rawcdn.githack.com/googlefonts/noto-cjk/165c01b46ea533872e002e0785ff17e44f6d97d8/Sans/OTF/Japanese/NotoSansCJKjp-Regular.otf',
+    );
+    return {
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    };
+  } else
+    return {
+      args: [],
+      // eslint-disable-next-line no-process-env
+      executablePath: process.env.CHROME_EXECUTABLE_PATH,
+      headless: true,
+    };
+};
 
 export async function getScreenshot(html: string, option: ParsedOptions) {
   const browserOptions = await getBrowserOption();
