06bf7c0c6be61293e7c7a7862537dddd60e3c10b
tosuke <tasukeprg@gmail.com>
2021-07-01 02:37:59 +0900

    feat: use puppeteer on local development
    See: https://github.com/alixaxel/chrome-aws-lambda/wiki/HOWTO:-Local-Development

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -1,27 +1,12 @@
 import chrome from 'chrome-aws-lambda';
-import core from 'puppeteer-core';
 import {ParsedOptions} from './parser';
 
-export const getBrowserOption = async () => {
-  // eslint-disable-next-line no-process-env
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    return {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-    };
-  } else
-    return {
-      args: [],
-      // eslint-disable-next-line no-process-env
-      executablePath: process.env.CHROME_EXECUTABLE_PATH,
-      headless: true,
-    };
-};
-
 export async function getScreenshot(html: string, option: ParsedOptions) {
-  const browserOptions = await getBrowserOption();
-  const browser = await core.launch(browserOptions);
+  const browser = await chrome.puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
 
   const page = await browser.newPage();
 
