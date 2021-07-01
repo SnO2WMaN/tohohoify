3d653f36673d58103d6b29263a67d1b415c016bc
SnO2WMaN <me@sno2wman.net>
2021-07-02 06:39:00 +0900

    fix: FontAwesomeのチラつきを多分修正 (#16)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
+import {config} from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import {AppProps} from 'next/app';
 import React from 'react';
 import '~/styles/global.css';
 import {DefaultLayout} from '../layouts/DefaultLayout';
+
+config.autoAddCss = false;
 
 export const App: React.FC<AppProps> = ({
   Component: PageComponent,
