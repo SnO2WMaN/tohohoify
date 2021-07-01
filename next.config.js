30a62e063f8cad85507a543054453e20a0513ed0
SnO2WMaN <me@sno2wman.net>
2021-07-02 06:08:40 +0900

    fix: 本番環境でのURL修正

--- next.config.js
+++ next.config.js
@@ -4,12 +4,12 @@ module.exports = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['localhost'],
+    domains: [process.env.APP_URL || process.env.VERCEL_URL],
   },
   env: {
     IMAGE_BASE_URL:
       process.env.NODE_ENV === 'development'
         ? `http://${process.env.VERCEL_URL}/api`
-        : `https://${process.env.VERCEL_URL}/api`,
+        : `https://${process.env.APP_URL || process.env.VERCEL_URL}/api`,
   },
 };
